0f12105eef0d847a3acd54185f6b2600bbeb9a0d
MACOY <valle.hernandez.16199@itsmante.edu.mx>
2021-05-03 20:42:49 -0500

    183

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   exports: [
-
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
